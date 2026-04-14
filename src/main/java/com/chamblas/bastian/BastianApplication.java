0307c3bf52253ea23cecc5e616424df30f54f880
MarcelCh-temp <bas.chamblas@duocuc.cl>
2026-04-14 13:22:06 -0400

    Primer Commit

---
+++ src/main/java/com/chamblas/bastian/BastianApplication.java
@@ -0,0 +1,13 @@
+package com.chamblas.bastian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BastianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BastianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chamblas/bastian/BastianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chamblas.bastian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BastianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
